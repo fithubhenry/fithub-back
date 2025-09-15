81481cd97e4ac3354fdb38dc02510d1df5aa1e28
MatiCassagne <matiascassagne@gmail.com>
2025-09-15 17:17:49 -0300

    Fix MP module

--- back/src/mercado-pago/mercado-pago.module.ts
+++ back/src/mercado-pago/mercado-pago.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PaymentsController } from './mercado-pago.controller';
 import { PaymentsService } from './mercado-pago.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [PaymentsController],
   providers: [PaymentsService],
 })
