0eacf7751eb6722a5cf03e3f2083b02bbd148391
MatiCassagne <matiascassagne@gmail.com>
2025-09-17 22:29:38 -0300

    fix filtros

--- back/src/clases/filtros.repository.ts
+++ back/src/clases/filtros.repository.ts
@@ -20,6 +20,12 @@ export class FiltrosRepository {
       });
     }
 
+    if (filtro.sede) {
+      queryBuilder.andWhere('clase.sede = :sede', {
+        intensidad: filtro.sede,
+      });
+    }
+
     if (filtro.tipo) {
       queryBuilder.andWhere('clase.tipo = :tipo', { tipo: filtro.tipo });
     }
