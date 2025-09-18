f3e445f5a91d59e45324c8e3a41f72e043e3a2ae
Sebastian <kamisama656@gmail.com>
2025-09-17 21:35:25 -0300

    Cambios schedule module

--- back/src/schedule/schedule.module.ts
+++ back/src/schedule/schedule.module.ts
@@ -5,11 +5,15 @@ import { TurnosService } from 'src/turno/turno.service';
 import { TurnosReminderService } from 'src/turno/turnoRecordatorio.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Turno } from 'src/turno/entities/turno.entity';
-import { User } from 'mercadopago';
+import { User } from 'src/user/entities/user.entity';
 import { Clase } from 'src/clases/entities/clase.entity';
+import { ScheduleModule as NestScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Turno, User, Clase])],
+  imports: [
+    NestScheduleModule.forRoot(),
+    TypeOrmModule.forFeature([Turno, User, Clase]),
+  ],
   controllers: [ScheduleController],
   providers: [ScheduleService, TurnosService, TurnosReminderService],
 })
