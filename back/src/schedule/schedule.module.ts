04c8d630c523cf3c3621f98e66a9a4cbec2720c0
Sebastian <kamisama656@gmail.com>
2025-09-17 21:10:49 -0300

    Me falto uno mas

--- back/src/schedule/schedule.module.ts
+++ back/src/schedule/schedule.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ScheduleService } from './schedule.service';
 import { ScheduleController } from './schedule.controller';
 import { TurnosService } from 'src/turno/turno.service';
+import { TurnosReminderService } from 'src/turno/turnoRecordatorio.service';
 
 @Module({
   controllers: [ScheduleController],
-  providers: [ScheduleService, TurnosService],
+  providers: [ScheduleService, TurnosService, TurnosReminderService],
 })
 export class ScheduleModule {}
