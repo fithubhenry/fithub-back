c4280cea5ad19c555a94028bdafbb72d1a7c6f9a
Sebastian <kamisama656@gmail.com>
2025-09-23 04:03:02 -0300

    Cambios 2

--- back/src/chat/chat.controller.ts
+++ back/src/chat/chat.controller.ts
@@ -21,6 +21,6 @@ export class ChatController {
     },
   })
   async sendMessage(@Body() body: ChatDto) {
-    return await this.chatService.sendMessage(body.message, body.context);
+    return await this.chatService.sendMessage(body.message);
   }
 }
