3606045f40444726fb990a6c860560b796c458ed
MatiCassagne <matiascassagne@gmail.com>
2025-09-23 00:48:52 -0300

    Agregado guard a /branches

--- back/src/branches/branches.controller.ts
+++ back/src/branches/branches.controller.ts
@@ -1,6 +1,10 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { BranchesService } from './branches.service';
 import { CreateBranchDto } from './dto/create-branch.dto';
+import { Roles } from 'src/decorators/roles.decorator';
+import { ERoles } from 'src/common/rolesEnum';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
 
 @Controller('branches')
 export class BranchesController {
@@ -11,6 +15,8 @@ export class BranchesController {
     return await this.branchesService.findAll();
   }
 
+  @Roles(ERoles.Admin)
+  @UseGuards(AuthGuard, RolesGuard)
   @Post()
   async create(@Body() sucursal: CreateBranchDto) {
     return await this.branchesService.addBranch(sucursal);
