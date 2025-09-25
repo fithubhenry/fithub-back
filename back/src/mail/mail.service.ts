b1ad41950cb1649418537ad3f209681e1654cde1
Sebastian <kamisama656@gmail.com>
2025-09-24 23:00:44 -0300

    Cambios import

--- back/src/mail/mail.service.ts
+++ back/src/mail/mail.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
-import * as sgMail from '@sendgrid/mail';
+import sgMail from '@sendgrid/mail';
 
 @Injectable()
 export class MailerService {
